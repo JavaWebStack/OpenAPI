3703f713e8fe2d8192a3cdcb942430cbf48655fe
JanHolger <jan@bebendorf.eu>
2020-12-21 23:11:31 +0100

    Added getMethods helper to OpenAPIPath

--- src/main/java/org/javawebstack/openapi/parser/OpenAPIPath.java
+++ src/main/java/org/javawebstack/openapi/parser/OpenAPIPath.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.javawebstack.openapi.parser.util.ParameterListAdapter;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -21,5 +23,22 @@ public class OpenAPIPath {
     OpenAPIOperation patch;
     OpenAPIOperation delete;
     OpenAPIOperation head;
+
+    public Map<String, OpenAPIOperation> getMethods(){
+        Map<String, OpenAPIOperation> methods = new HashMap<>();
+        if(get != null)
+            methods.put("get", get);
+        if(post != null)
+            methods.put("post", post);
+        if(put != null)
+            methods.put("put", put);
+        if(patch != null)
+            methods.put("patch", patch);
+        if(delete != null)
+            methods.put("delete", delete);
+        if(head != null)
+            methods.put("head", head);
+        return methods;
+    }
 
 }
