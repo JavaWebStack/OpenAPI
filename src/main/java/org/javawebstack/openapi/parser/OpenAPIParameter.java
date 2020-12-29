7d8c15e09120b3d7f757ce49b2e656e2cb915ee8
JanHolger <jan@bebendorf.eu>
2020-12-30 00:21:53 +0100

    Switched deployment to LUMASERV Nexus

--- src/main/java/org/javawebstack/openapi/parser/OpenAPIParameter.java
+++ src/main/java/org/javawebstack/openapi/parser/OpenAPIParameter.java
@@ -2,14 +2,20 @@ package org.javawebstack.openapi.parser;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.annotations.JsonAdapter;
 import lombok.Getter;
 import lombok.Setter;
+import org.javawebstack.graph.util.GsonEnum;
+import org.javawebstack.graph.util.GsonEnumAdapter;
+
+import java.util.Locale;
 
 @Getter @Setter
 public class OpenAPIParameter {
 
     String name;
     String description;
+    @JsonAdapter(GsonEnumAdapter.class)
     Location in;
     OpenAPISchema schema;
     Boolean required;
@@ -18,11 +24,14 @@ public class OpenAPIParameter {
     JsonArray examples;
     Boolean deprecated;
 
-    public enum Location {
+    public enum Location implements GsonEnum {
         PATH,
         QUERY,
         HEADER,
-        COOKIE
+        COOKIE;
+        public String gsonValue() {
+            return name().toLowerCase(Locale.ROOT);
+        }
     }
 
 }
